40a47ede89e2d8de7d3223f34bb287eea6b2afcf
Angelo <Angelodimauro91@gmail.com>
2022-10-14 00:06:37 +0200

    End

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 
+<?php
 
+ $ParolaUtente = $_GET['word'];
+
+
+$paragrafo = "Fin che la barca va, lasciala affondare";
+
+
+$lunghezza = strlen($paragrafo);
+
+$censoredParagrafo = str_ireplace($ParolaUtente, '***', $paragrafo);
+$censoredLenght = strlen($censoredParagrafo);
+
+?>
+
+
+
+
+<div>
+    <h3>Parola: </h3>              <p><?= $paragrafo ?></p> 
+    <h3>lunghezza parola: </h3>    <p><?= $lunghezza ?></p>
+    <h3>Parola dell'utente</h3>    <p><?= $ParolaUtente ?></p>
+    <h3>Parola censurata</h3>      <p><?= $censoredParagrafo ?></p>
+</div>
 
 
 
